5714e49a63ac3b0f45c2b57f6e005e65335732cf
dpazmino94 <davidsebastianpc@gmail.com>
2022-01-10 07:11:29 -0500

    Generating custom link with timestamp, id and token

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import React, { useState } from "react";
 import { ResourcePicker } from "@shopify/app-bridge-react";
 
 const NGROK_SERVER =
-  "https://340d73a79ed6.ngrok.io//david-pazmino-server/us-central1/app/shopify/admin-url";
+  "https://340d73a79ed6.ngrok.io/david-pazmino-server/us-central1/app/shopify/admin-url";
 
-const generateURL = (variantID, timeStamp) => {
-  return `${NGROK_SERVER}?tms=${timeStamp}&id=${variantID}`;
+const generateURL = (variantID, timeStamp, token) => {
+  return `${NGROK_SERVER}?tms=${timeStamp}&id=${variantID}&tk=${token}`;
 };
 
 const Index = () => {
@@ -25,9 +25,10 @@ const Index = () => {
         open={open}
         onSelection={(data) => {
           let variantID = data.selection[0].variants[0].id,
-            timeStamp = new Date().valueOf();
+            timeStamp = new Date().valueOf(),
+            token = Number(timeStamp.toString().substring(0, 3)) - 10;
           variantID = variantID.replace(removableString, "");
-          setGeneratedURL(generateURL(variantID, timeStamp));
+          setGeneratedURL(generateURL(variantID, timeStamp, token));
           setOpen(false);
         }}
       />
