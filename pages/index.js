657aa6563d1302ce3ae2f252a4718b564cd04fb8
dpazmino94 <davidsebastianpc@gmail.com>
2022-01-10 06:35:20 -0500

    Generating first test of the link

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,42 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Page } from "@shopify/polaris";
+import React, { useState } from "react";
+import { ResourcePicker } from "@shopify/app-bridge-react";
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+const NGROK_SERVER =
+  "https://340d73a79ed6.ngrok.io//david-pazmino-server/us-central1/app/shopify/admin-url";
+
+const generateURL = (variantID, timeStamp) => {
+  return `${NGROK_SERVER}?tms=${timeStamp}&id=${variantID}`;
+};
+
+const Index = () => {
+  const [open, setOpen] = useState(false);
+  const [generatedURL, setGeneratedURL] = useState("-");
+  let removableString = "gid://shopify/ProductVariant/";
+
+  return (
+    <Page title="Product Selector">
+      <button onClick={() => setOpen(true)}>Generate link for product</button>
+      <br /> <br />
+      <div>Generated URL:</div> <br />
+      {generatedURL}
+      <ResourcePicker
+        resourceType="Product"
+        open={open}
+        onSelection={(data) => {
+          let variantID = data.selection[0].variants[0].id,
+            timeStamp = new Date().valueOf();
+          variantID = variantID.replace(removableString, "");
+          setGeneratedURL(generateURL(variantID, timeStamp));
+          setOpen(false);
+        }}
+      />
+    </Page>
+  );
+};
 
 export default Index;
+
+// https://deviget-challenge.myshopify.com/cart/add?id=7526176358638&quantity=1
+
+// https://deviget-challenge.myshopify.com/cart/add?id=42305603666158&quantity=1
